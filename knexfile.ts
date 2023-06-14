763a7da08d63da0ad3ee825236969148551bcbd2
mardoquudickson1 <mardoqueudrums1999@gmail.com>
2023-06-14 03:25:48 +0100

    fixed not allow by cors 5

--- knexfile.ts
+++ knexfile.ts
@@ -4,28 +4,28 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const PORT_DB: number = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 0;
+const PORT_DB: number = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 0;
 
-// const developmentConfig: Knex.Config = {
-//   client: 'postgresql',
-//   connection: {
-//     host: 'localhost',
-//     port: PORT_DB,
-//     user: process.env.DB_USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//   },
+const developmentConfig: Knex.Config = {
+  client: 'postgresql',
+  connection: {
+    host: 'localhost',
+    port: PORT_DB,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  },
 
-//   migrations: {
-//     tableName: 'knex_migrations',
-//     directory: path.join(__dirname, 'src', 'database', 'migrations'),
-//   },
-//   seeds: {
-//     directory: path.join(__dirname, 'src', 'database', 'seeds'),
-//   },
-// };
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: path.join(__dirname, 'src', 'database', 'migrations'),
+  },
+  seeds: {
+    directory: path.join(__dirname, 'src', 'database', 'seeds'),
+  },
+};
 
-const config: Knex.Config = {
+const productionConfig: Knex.Config = {
   client: 'postgresql',
   connection: process.env.DATABASE_URL,
 
@@ -38,6 +38,6 @@ const config: Knex.Config = {
   },
 };
 
-// const config: Knex.Config = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
+const config: Knex.Config = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
 
 export default config;
